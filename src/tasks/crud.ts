9e3d091f3c614ac814bdc15f332563255d44bead
Jose Zanichelli <mjzzanichelli@gmail.com>
2024-07-27 17:42:10 +0100

    renamed todo as tasks

--- src/tasks/crud.ts
+++ src/tasks/crud.ts
@@ -2,7 +2,7 @@ import { client } from "./../amplify";
 import { TaskDataType } from "./meta";
 
 export async function createTask() {
-  const { data, errors } = await client.models.Todo.create({
+  const { data, errors } = await client.models.Tasks.create({
     name: "New Task",
   });
   if (errors) return;
@@ -10,13 +10,13 @@ export async function createTask() {
 }
 
 export async function getTask(id: string) {
-  const { data, errors } = await client.models.Todo.get({ id });
+  const { data, errors } = await client.models.Tasks.get({ id });
   if (errors) return;
   return data;
 }
 
 export async function listTasks() {
-  const { data, errors } = await client.models.Todo.list();
+  const { data, errors } = await client.models.Tasks.list();
   if (errors) return;
   return data;
 }
@@ -24,13 +24,13 @@ export async function listTasks() {
 export async function updateTask(
   task: Partial<TaskDataType> & Pick<TaskDataType, "id">
 ) {
-  const { data, errors } = await client.models.Todo.update(task);
+  const { data, errors } = await client.models.Tasks.update(task);
   if (errors) return;
   return data;
 }
 
 export async function deleteTask(task: Pick<TaskDataType, "id">) {
-  const { data, errors } = await client.models.Todo.delete(task);
+  const { data, errors } = await client.models.Tasks.delete(task);
   if (errors) return;
   return data;
 }
