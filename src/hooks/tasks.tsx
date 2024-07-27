7fea9b9edd15e3330dc786bed4bfe1f290aae034
Jose Zanichelli <mjzzanichelli@gmail.com>
2024-07-27 17:30:24 +0100

    added task name update

--- src/hooks/tasks.tsx
+++ src/hooks/tasks.tsx
@@ -4,7 +4,14 @@ import {
   StyledTableCellStyckyRight,
 } from "../components/table/styled";
 import { CheckBox } from "../components/form/input";
-import { TaskDataType, TasksMeta, TasksMetaType } from "../tasks/meta";
+import {
+  getMetaActions,
+  getMetaCheck,
+  MetaCheckLabel,
+  TaskDataType,
+  TasksMeta,
+  TasksMetaType,
+} from "../tasks/meta";
 import { deleteTask, listTasks, updateTask } from "../tasks/crud";
 import { Button } from "../components/button/main";
 import { Icon } from "../components/icon/main";
@@ -29,91 +36,39 @@ export function useTasksMeta() {
     refreshTasks();
   }, [refreshTasks]);
 
-  const checkMeta: TasksMetaType = {
-    key: "done",
-    thStyled: (
-      <StyledTableCellStyckyLeft align="center" style={{ width: "2.5rem" }} />
-    ),
-    tdStyled: <StyledTableCellStyckyLeft align="center" />,
-    value: (task) => {
-      const { id } = task;
-      let { done } = task;
-      done = done ?? false;
-      return (
-        <CheckBox
-          defaultChecked={done}
-          onChange={() => {
-            updateTask({ id, done: !done }).then(refreshTasks);
-          }}
-        />
-      );
+  const makeEditable = useCallback(
+    (task: TaskDataType) => {
+      if (!tasks) return;
+      if (!task.editable) {
+        task.editable = !task.editable;
+        setTasks([...tasks]);
+      } else {
+        const { id, name } = task;
+        updateTask({ id, name }).then(refreshTasks);
+      }
     },
-  };
+    [tasks, refreshTasks]
+  );
 
-  const actionsMeta: TasksMetaType = {
-    key: "actions",
-    label: "Actions",
-    thStyled: (
-      <StyledTableCellStyckyRight align="center" style={{ width: "6rem" }} />
-    ),
-    tdStyled: <StyledTableCellStyckyRight align="center" />,
-    value: (task) => {
-      const { id } = task;
-      let { done } = task;
-      done = done ?? false;
-      return (
-        <>
-          <Button
-            onClick={() => {
-              deleteTask({ id }).then(refreshTasks);
-            }}
-          >
-            <Icon name="xmark" />
-          </Button>
-        </>
-      );
-    },
-  };
+  const metaCheck = getMetaCheck(refreshTasks);
+
+  const metaActions = getMetaActions({ makeEditable, refreshTasks });
 
   const checkMetaTodo: TasksMetaType = {
-    ...checkMeta,
-    label: (
-      <CheckBox
-        key={Math.random()}
-        defaultChecked={false}
-        onChange={() => {
-          const todo = tasks?.filter((item) => !item.done);
-          if (!todo?.length) return;
-          Promise.all(
-            todo.map(({ id }) => updateTask({ id, done: true }))
-          ).then(refreshTasks);
-        }}
-      />
-    ),
+    ...metaCheck,
+    label: <MetaCheckLabel tasks={tasks} refreshTasks={refreshTasks} done />,
   };
 
   const checkMetaDone: TasksMetaType = {
-    ...checkMeta,
-    label: (
-      <CheckBox
-        key={Math.random()}
-        defaultChecked={false}
-        onChange={() => {
-          const done = tasks?.filter((item) => !!item.done);
-          if (!done?.length) return;
-          Promise.all(
-            done.map(({ id }) => updateTask({ id, done: false }))
-          ).then(refreshTasks);
-        }}
-      />
-    ),
+    ...metaCheck,
+    label: <MetaCheckLabel tasks={tasks} refreshTasks={refreshTasks} />,
   };
 
   return {
     tasks,
     setTasks,
     refreshTasks,
-    metaTodo: [checkMetaTodo, ...TasksMeta, actionsMeta],
-    metaDone: [checkMetaDone, ...TasksMeta, actionsMeta],
+    metaTodo: [checkMetaTodo, ...TasksMeta, metaActions],
+    metaDone: [checkMetaDone, ...TasksMeta, metaActions],
   };
 }
--- src/tasks/meta.tsx
+++ src/tasks/meta.tsx
@@ -1,14 +1,20 @@
-import { CheckBox } from "../components/form/input";
+import { Button } from "../components/button/main";
+import { FieldComponent } from "../components/form/field";
+import { CheckBox, Input } from "../components/form/input";
+import { Icon } from "../components/icon/main";
 import {
   StyledTableCell,
+  StyledTableCellStyckyLeft,
   StyledTableCellStyckyRight,
 } from "../components/table/styled";
 import { TableDataType, TableMetaType } from "../components/table/types";
+import { deleteTask, updateTask } from "./crud";
 
 export interface TaskDataType extends TableDataType {
   id: string;
   name: string;
   done?: boolean;
+  editable?: boolean;
 }
 
 export type TasksMetaType = TableMetaType<TaskDataType>;
@@ -18,6 +24,17 @@ export const TasksMeta: TasksMetaType[] = [
     key: "name",
     label: "Task name",
     thStyled: <StyledTableCell style={{ width: "50%" }} />,
+    value: function (task) {
+      if (!task.editable) return task.name;
+      return (
+        <Input
+          defaultValue={task.name}
+          onChange={(value) => {
+            task.name = value as string;
+          }}
+        />
+      );
+    },
   },
   {
     key: "date",
@@ -32,3 +49,86 @@ export const TasksMeta: TasksMetaType[] = [
     label: "Note",
   },
 ];
+
+export function getMetaCheck(refreshTasks: () => void): TasksMetaType {
+  return {
+    key: "done",
+    thStyled: (
+      <StyledTableCellStyckyLeft align="center" style={{ width: "2.5rem" }} />
+    ),
+    tdStyled: <StyledTableCellStyckyLeft align="center" />,
+    value: (task) => {
+      const { id } = task;
+      let { done } = task;
+      done = done ?? false;
+      return (
+        <CheckBox
+          key={id}
+          defaultChecked={done}
+          onChange={() => {
+            updateTask({ id, done: !done }).then(refreshTasks);
+          }}
+        />
+      );
+    },
+  };
+}
+
+export function MetaCheckLabel(args: {
+  tasks?: TaskDataType[];
+  done?: boolean;
+  refreshTasks: () => void;
+}) {
+  const { tasks, done = false, refreshTasks } = args;
+  return (
+    <CheckBox
+      key={Math.random()}
+      defaultChecked={false}
+      onChange={() => {
+        const todo = tasks?.filter((item) => !!item.done != done);
+        if (!todo?.length) return;
+        Promise.all(todo.map(({ id }) => updateTask({ id, done }))).then(
+          refreshTasks
+        );
+      }}
+    />
+  );
+}
+
+export function getMetaActions(args: {
+  makeEditable: (task: TaskDataType) => void;
+  refreshTasks: () => void;
+}): TasksMetaType {
+  const { makeEditable, refreshTasks } = args;
+  return {
+    key: "actions",
+    label: "Actions",
+    thStyled: (
+      <StyledTableCellStyckyRight align="center" style={{ width: "6rem" }} />
+    ),
+    tdStyled: <StyledTableCellStyckyRight align="center" />,
+    value: (task) => {
+      const { id } = task;
+      let { done } = task;
+      done = done ?? false;
+      return (
+        <>
+          <Button
+            onClick={() => {
+              makeEditable(task);
+            }}
+          >
+            <Icon name="xmark" />
+          </Button>
+          <Button
+            onClick={() => {
+              deleteTask({ id }).then(refreshTasks);
+            }}
+          >
+            <Icon name="xmark" />
+          </Button>
+        </>
+      );
+    },
+  };
+}
